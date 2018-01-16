b271becb4fa2f8cb1b68633e27ba07c6db651e8e
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-16 19:10:55 +0700

    add script for change bitmap to text file

---
+++ scripts/bitmap.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: bitmap.py filename")
+    sys.exit(-1)
+
+filename = sys.argv[1]
+
+im = Image.open(filename, 'r')
+im = im.resize((20, 20), Image.ANTIALIAS)
+pic = im.load()
+
+for y in range(20):
+    arr = []
+    for x in range(20):
+        r, g, b = pic[x,y]
+        color = (r + g + b) / 3.0
+        if color > 200:
+            arr.append('0')
+        else:
+            arr.append('1')
+    print("".join(arr))

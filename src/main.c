150eef1d58034c11188b2c7779e59fdc5b71e553
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-16 18:08:54 +0700

    add back buffer as double buffering technique

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <linux/fb.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <string.h>
 
 uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo);
 void put_pixel_color(uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo);
+void put_image_color(char* file, uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo);
 
 void init_framebuffer_setting(int* fb_fd, struct fb_fix_screeninfo* fix_screen_info,
                               struct fb_var_screeninfo* var_screen_info) {
@@ -34,11 +37,12 @@ int main() {
     struct fb_var_screeninfo var_screen_info;
     struct fb_fix_screeninfo fix_screen_info;
     int fb_fd;
-    uint8_t *fbp;
+    uint8_t *fbp, *backbuff;
 
     init_framebuffer_setting(&fb_fd, &fix_screen_info, &var_screen_info);
     long screensize = var_screen_info.yres_virtual * fix_screen_info.line_length;
     fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t) 0);
+    backbuff = (uint8_t*) malloc(var_screen_info.yres_virtual * fix_screen_info.line_length);
 
     int cur_y = 0;
     while (1) {
@@ -46,13 +50,16 @@ int main() {
         int x,y;
         for (x = 0; x < var_screen_info.xres; x++)
             for (y = 0; y < var_screen_info.yres; y++)
-                put_pixel_color(fbp, x, y, 0x0, 0, 0x0, &var_screen_info, &fix_screen_info);
+                put_pixel_color(backbuff, x, y, 0x0, 0, 0x0, &var_screen_info, &fix_screen_info);
         
         for (x = var_screen_info.xres/8*3; x < var_screen_info.xres/8*5; x++)
-            put_pixel_color(fbp, x, cur_y, 0xff, 0xff, 0xff, &var_screen_info, &fix_screen_info);
+            put_pixel_color(backbuff, x, cur_y, 0xff, 0xff, 0xff, &var_screen_info, &fix_screen_info);
+        put_image_color("data/a.txt", backbuff, 10, 10, 0xff, 0xff, 0xff, &var_screen_info, &fix_screen_info);
         
+        memcpy(fbp, backbuff, var_screen_info.yres_virtual * fix_screen_info.line_length);
+
         cur_y = (cur_y + 1) % var_screen_info.yres;
-        usleep(10000);
+        usleep(1000);
     }
 
 	return 0;
@@ -66,4 +73,18 @@ void put_pixel_color(uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b
     uint32_t value = pixel_color(r, g, b, vinfo);
     long mem_loc = (x + vinfo->xoffset) * (vinfo->bits_per_pixel/8) + (y + vinfo->yoffset) * finfo->line_length;
     *((uint32_t*) (mem + mem_loc)) = value;
+}
+
+void put_image_color(char* file, uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo) {
+    FILE* fd = fopen(file, "r");
+    char* s = (char*) malloc(256);
+    while (fscanf(fd, "%s", s) != EOF ) {
+        int i;
+        for (i = 0; i < strlen(s); i++)
+            if (s[i] == '1')
+                put_pixel_color(mem, x + i, y, r, g, b, vinfo, finfo);
+        y++;    
+    }
+    free(s);
+    fclose(fd);
 }

638e46b458de2a42add167fb7f325ae110503c30
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-21 23:13:27 +0700

    add custom text in program

--- src/main.c
+++ src/main.c
@@ -8,7 +8,8 @@
 #include <unistd.h>
 #include <string.h>
 
-const char* title_text = "Hello World";
+const char* title_text = "Dery Rahman";
+const int char_width = 15;
 
 uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo);
 void put_pixel_color(uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo);
@@ -35,7 +36,10 @@ void init_framebuffer_setting(int* fb_fd, struct fb_fix_screeninfo* fix_screen_i
         *fix_screen_info = finfo;
 }
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc >= 2)
+        title_text = argv[1];
+    
     struct fb_var_screeninfo var_screen_info;
     struct fb_fix_screeninfo fix_screen_info;
     int fb_fd;
@@ -61,13 +65,13 @@ int main() {
         for (i = 0; i < strlen(title_text); i++) {
             char* filename = (char*) malloc(256);
             sprintf(filename, "data/%c.txt", title_text[i]);
-            put_image_color(filename, backbuff, 10+22*i, cur_y, 0xff, 0xff, 0xff, &var_screen_info, &fix_screen_info);
+            put_image_color(filename, backbuff, 10+(char_width + 2)*i, cur_y, 0xff, 0x00, 0xff, &var_screen_info, &fix_screen_info);
             free(filename);
         }
         
         memcpy(fbp, backbuff, var_screen_info.yres_virtual * fix_screen_info.line_length);
 
-        cur_y = (cur_y + 4) % (var_screen_info.yres - 20);
+        cur_y = (cur_y + 4) % (var_screen_info.yres - char_width);
         usleep(10);
     }
 
@@ -86,6 +90,7 @@ void put_pixel_color(uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b
 
 void put_image_color(char* file, uint8_t* mem, int x, int y, uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo) {
     FILE* fd = fopen(file, "r");
+    if (fd < 0) return;
     char* s = (char*) malloc(256);
     while (fscanf(fd, "%s", s) != EOF ) {
         int i;

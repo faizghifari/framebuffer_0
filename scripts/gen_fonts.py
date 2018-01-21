656449d2e40dbe22e5c07904c1b6b0f2070e7ab9
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-21 23:14:15 +0700

    resize font to 15x20 in generate font script

--- scripts/gen_fonts.py
+++ scripts/gen_fonts.py
@@ -11,8 +11,8 @@ filename = sys.argv[1]
 for c in list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789? '):
     img = Image.new("RGBA", (40,40),(255,255,255))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(filename, 24)
-    draw.text((0,0), c, font=font, fill="black")
+    font = ImageFont.truetype(filename, 40)
+    draw.text((8,0), c, font=font, fill="black")
     
     # pic = img.load()
     # upper_bound = (40, 40)
@@ -26,14 +26,14 @@ for c in list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789? '
     #             lower_bound = max(lower_bound[0], x), max(lower_bound[1], y)
 
     # img = img.crop((upper_bound[0], upper_bound[1], lower_bound[0], lower_bound[1]))
-    img = img.resize((20, 20), Image.ANTIALIAS)
+    img = img.resize((15, 20), Image.ANTIALIAS)
     pic = img.load()
 
     file_string = ""
 
     for y in range(20):
         arr = []
-        for x in range(20):
+        for x in range(15):
             r,g,b,a = pic[x,y]
             color = (r + g + b) / 3.0
             if color > 200:

e75c1842efe5f51f2ea928bace36092125dddb26
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-25 08:37:02 +0700

    add bersenham case when y coordinate decrease

--- src/screen_util.c
+++ src/screen_util.c
@@ -1,4 +1,5 @@
 #include "screen_util.h"
+#include <stdlib.h>
 
 unsigned int pixel_color(unsigned char r, unsigned char g, unsigned char b) {
     return (r<<16) | (g<<8) | b;
@@ -15,7 +16,7 @@ void draw_line(screen* scr, int x1, int y1, int x2, int y2) {
     }
     char inc = y2 >= y1 ? 1 : 0;
     int dx = x2 - x1;
-    int dy = y2 - y1;
+    int dy = abs(y2 - y1);
     int e = 0;
     for (;x1 <= x2; x1++) {
         if (e <= 0) {

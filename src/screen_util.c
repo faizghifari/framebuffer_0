6d510c72317bb42fc219caacbf7c4e43cd8d8a2d
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-25 08:19:22 +0700

    add draw line utility function using bersenham algorithm, only for case 1

--- src/screen_util.c
+++ src/screen_util.c
@@ -3,3 +3,18 @@
 unsigned int pixel_color(unsigned char r, unsigned char g, unsigned char b) {
     return (r<<16) | (g<<8) | b;
 }
+
+void draw_line(screen* scr, int x1, int y1, int x2, int y2) {
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int e = 0;
+    for (;x1 <= x2; x1++) {
+        if (e <= 0) {
+            put_pixel(scr, x1, y1, 0xffffff);
+            e = e + 2*dy;
+        } else {
+            put_pixel(scr, x1, ++y1, 0xffffff);
+            e = e + 2*dy - 2*dx;
+        }
+    }
+}
--- src/screen_util.h
+++ src/screen_util.h
@@ -5,4 +5,6 @@
 
 unsigned int pixel_color(unsigned char r, unsigned char g, unsigned char b);
 
+void draw_line(screen* scr, int x1, int y1, int x2, int y2);
+
 #endif

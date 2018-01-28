00b05996735e51c934a21ae4e61ed93fe9a96819
Jauhar Arifin <jauhararifin10@gmail.com>
2018-01-28 15:55:16 +0700

    update main

--- src/main.c
+++ src/main.c
@@ -8,59 +8,66 @@
 #include <unistd.h>
 #include <string.h>
 #include "screen.h"
+#include "image.h"
 #include "screen_util.h"
 
-const int char_width = 15;
-#define MAX_TXT_SZ 256
-#define UP -4
-#define DOWN 4
-// const char* title_text = "Hello World";
-
-void put_image_color(char* file, screen* scr, int x, int y, uint8_t r, uint8_t g, uint8_t b);
 void clear_screen(screen* scr);
-void animate_text(screen* scr, char* title_text, int* cur_y, int opt);
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage: main <string-to-show>");
+        return -1;
+    }
+    char* message = argv[1];
+
     screen scr;
     init_screen(&scr);
+    int width, height;
+    get_screen_height(&scr, &height);
+    get_screen_width(&scr, &width);
 
-    int cur_y = 0;
-    int opt = DOWN;
-    char c_opt;
-
-    // input
-    char title_text[MAX_TXT_SZ];
-    printf("Enter text : ");
-    fgets(title_text, MAX_TXT_SZ, stdin);
-    printf("up/down? (u/D) : ");
-    scanf("%c", &c_opt);
-    if(c_opt == 'u' || c_opt == 'U') { opt = UP; }
-    if ((strlen(title_text) > 0) && (title_text[strlen (title_text) - 1] == '\n'))
-        title_text[strlen (title_text) - 1] = '\0';
+    int top = 0;
     while (1) {
-        // clear screen
         clear_screen(&scr);
-        animate_text(&scr, title_text, &cur_y, opt);
+
+        int i;
+        int msg_n = strlen(message);
+        for (i = 0; i < msg_n; i++) {
+            char c = message[i];
+            char filename[256];
+            sprintf(filename, "data/%d.txt", (int) c);
+            
+            image img;
+            if (load_image_from_file(filename, &img) > -1)
+                draw_image(&scr, (width - msg_n * 20)/2 + 20*i, top, 0xffffff, img);
+        }
+
+        flush_screen(&scr);
         usleep(1);
+        top = (top + 3) % height;
     }
+
+    // while (1) {
+    //     draw_line(&scr, 100, 100, 800, 150); // top left -> bottom right
+    //     draw_line(&scr, 800, 160, 100, 110); // bottom right -> top left
+    //     draw_line(&scr, 900, 100, 1600, 50); // bottom left -> top right
+    //     draw_line(&scr, 1600, 60, 900, 110); // top right -> bottom left
+    //     draw_line(&scr, 400, 200, 1200, 200); // left -> right
+    //     draw_line(&scr, 1200, 210, 400, 210); // right -> left
+
+    //     draw_line(&scr, 100, 300, 200, 600); // top left -> bottom right
+    //     draw_line(&scr, 210, 600, 110, 300); // bottom right -> top left
+    //     draw_line(&scr, 300, 300, 200, 600); // top right -> bottom left
+    //     draw_line(&scr, 260, 600, 360, 300); // bottom left -> top right
+    //     draw_line(&scr, 270, 300, 270, 600); // top -> bottom
+    //     draw_line(&scr, 280, 600, 280, 300); // bottom -> top
+        
+    //     flush_screen(&scr);
+    //     usleep(1);
+    // }
     free_screen(&scr);
 
     return 0;
-}
-
-void put_image_color(char* file, screen* scr, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-    FILE* fd = fopen(file, "r");
-    if (fd < 0) return;
-    char* s = (char*) malloc(256);
-    while (fscanf(fd, "%s", s) != EOF ) {
-        int i;
-        for (i = 0; i < strlen(s); i++)
-            if (s[i] == '1')
-                put_pixel(scr, x + i, y, pixel_color(r,g,b));
-        y++;    
-    }
-    free(s);
-    fclose(fd);
 }
 
 void clear_screen(screen* scr){
@@ -71,23 +78,4 @@ void clear_screen(screen* scr){
     for (x = 0; x < width; x++)
         for (y = 0; y < height; y++)
             put_pixel(scr, x, y, 0);
-}
-
-void animate_text(screen* scr, char* title_text, int* cur_y, int opt){
-    int i, width, height;
-    get_screen_height(scr, &height);
-    get_screen_width(scr, &width);
-
-    long mid = (width - (strlen(title_text) * char_width)) / 2;
-    for (i = 0; i < strlen(title_text); i++) {
-        char* filename = (char*) malloc(256);
-        sprintf(filename, "data/%c.txt", title_text[i]);
-        put_image_color(filename, scr, mid+(char_width+2)*i, *cur_y, 0xff, 0xff, 0xff);
-        free(filename);
-    }
-    flush_screen(scr);
-
-    if(opt == UP)
-        *cur_y += height - char_width;
-    *cur_y = (*cur_y + opt) % (height - char_width);
 }

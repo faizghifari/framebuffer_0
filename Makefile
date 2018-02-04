052e50bb7eea9f1b2f802c6e1286f8970c0bc8ce
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-04 22:35:21 +0700

    add main for task 3

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-all:
+all: task3
 	gcc src/main.c src/screen_util.c src/image.c src/screen.c -I src -o bin/main -lm -g
+
+task3:
+	gcc src/main_3.c src/screen_util.c src/image.c src/screen.c -I src -o bin/main_3 -lm -g
---
+++ src/main_3.c
@@ -0,0 +1,70 @@
+#include <linux/fb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include <termios.h>
+
+#include "screen.h"
+#include "image.h"
+#include "screen_util.h"
+
+void clear_screen(screen* scr);
+void put_char_to_screen(screen* scr, char c, int x, int y);
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage: main <string-to-show>");
+        return -1;
+    }
+    char* message = argv[1];
+
+    screen scr;
+    init_screen(&scr);
+    int width, height;
+    get_screen_height(&scr, &height);
+    get_screen_width(&scr, &width);
+
+    int top = 0;
+    while (1) {
+        clear_screen(&scr);
+
+        int i;
+        for (i = 0; i < (int) strlen(message); i++)
+            put_char_to_screen(&scr, message[i], 10 + i * 25, 10);
+        flush_screen(&scr);
+
+        usleep(10);
+    }
+
+    free_screen(&scr);
+
+    return 0;
+}
+
+void put_char_to_screen(screen* scr, char c, int x, int y) {
+    char fname[256];
+    sprintf(fname, "data/%d.txt", (int) c);
+    
+    if (c == 'J' || c == 'R' || c == 'A' || c == 'F' || c == 'I' || c == 'K')
+        sprintf(fname, "data/%c.txt", c);
+
+    image i_image;
+    load_image_from_file(fname, &i_image);
+    draw_image(scr, x, y, 0xffffff, i_image);
+}
+
+void clear_screen(screen* scr) {
+    int x, y, width, height;
+    get_screen_height(scr, &height);
+    get_screen_width(scr, &width);
+    
+    for (x = 0; x < width; x++)
+        for (y = 0; y < height; y++)
+            put_pixel(scr, x, y, 0);
+}

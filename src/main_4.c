d7bb92b5ffd95fe3427c85369629aeaa4c43a5c3
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-11 22:47:13 +0700

    add parachute logic

--- src/main_4.c
+++ src/main_4.c
@@ -25,6 +25,10 @@ typedef struct {
     double x, y, t;
 } explosion_t;
 
+typedef struct  {
+    double x, y, v;
+} parachute_t;
+
 void draw_plane(screen* scr, int x, int y, double t) {
     static const int PLANE_WIDTH = 40;
     static const int PLANE_HEIGHT = 8;
@@ -120,6 +124,10 @@ void draw_explosion(screen* scr, int x, int y, double t) {
     }
 }
 
+void draw_parachute(screen* scr, int x, int y) {
+    draw_line(scr, x, y, x, y + 10, 0xffffff);
+}
+
 void update_plane(screen* scr, int* n_plane, plane_t* plane_arr) {
     // get screen width and height
     int width, height;
@@ -163,6 +171,27 @@ void update_explosion(int *n_explosion, explosion_t* explosion_arr) {
     *n_explosion -= delete_num;
 }
 
+void update_parachute(screen *scr, int *n_parachute, parachute_t* parachute_arr) {
+    // get screen width and height
+    int width, height;
+    get_screen_height(scr, &height);
+    get_screen_width(scr, &width);
+
+    for (int i = 0; i < *n_parachute; i++) {
+        parachute_arr[i].v -= 0.25;
+        parachute_arr[i].y -= parachute_arr[i].v;
+    }
+    
+    // delete unused parachute
+    int delete_num = 0;
+    for (int i = 0; i < *n_parachute; i++)
+        if (parachute_arr[i].y > height)
+            delete_num++;
+        else
+            parachute_arr[i - delete_num] = parachute_arr[i];
+    *n_parachute -= delete_num;
+}
+
 int main(int argc, char** argv) {
     screen scr;
     init_screen(&scr);
@@ -176,6 +205,9 @@ int main(int argc, char** argv) {
     explosion_t* explosion_arr = (explosion_t*) malloc(MAX_PLANE * 5 * sizeof(explosion_t));
     int n_explosion = 0;
 
+    parachute_t* parachute_arr = (parachute_t*) malloc(MAX_PLANE * 5 * sizeof(parachute_t));
+    int n_parachute = 0;
+
     while (1) {
         clear_screen(&scr);
 
@@ -184,8 +216,11 @@ int main(int argc, char** argv) {
             draw_plane(&scr, plane_arr[i].x, plane_arr[i].y, plane_arr[i].t);
         for (i = 0; i < n_explosion; i++)
             draw_explosion(&scr, explosion_arr[i].x, explosion_arr[i].y, explosion_arr[i].t);
+        for (i = 0; i < n_parachute; i++)
+            draw_parachute(&scr, parachute_arr[i].x, parachute_arr[i].y);
         update_plane(&scr, &n_plane, plane_arr);
         update_explosion(&n_explosion, explosion_arr);
+        update_parachute(&scr, &n_parachute, parachute_arr);
         
         flush_screen(&scr);
         usleep(5000);
